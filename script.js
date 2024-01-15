9f75f44ba389153e2c96d5c9d7e80beaf731da60
S1ardor <quvondiqovs06@gmail.com>
2024-01-15 21:30:40 +1000

    JS lesson-2

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-var userName = prompt("Ismingizni kriting").trim();
-var userSourName = prompt("Familiyangizni kriting").trim();
-var userAge = Number(prompt("Yoshingizni kriting"));
-var userEmail = prompt("Emailingizni kriting").trim();
-var userPhoneNumber = Number(prompt("Telefon nomeringizni kriting"));
-var userSum = Number(Math.abs(prompt("Sizni necha yuz dollor bor")));
-var DOLLOR_UZS = 12400;
-var sum = userSum * DOLLOR_UZS;
+// * * * 1-mashq * * * //
 
-console.log(`Name: ${userName}
-Sourname: ${userSourName}
-Age: ${userAge}
-Email: ${userEmail}
-Phone: ${userPhoneNumber}
-Sizning UZB somingiz: ${sum}`);
+var FizzBuzzTask = Number(Math.abs(prompt("Son kriting")));
+
+if(FizzBuzzTask == ""){
+    console.log("Son kritishingiz majburiy");
+}else if (FizzBuzzTask % 3 == 0 && FizzBuzzTask % 5 == 0) {
+  console.log("FizzBUZZ");
+} else if (FizzBuzzTask % 3 == 0) {
+  console.log("Fizz");
+} else if (FizzBuzzTask % 5 == 0) {
+  console.log("Buzz");
+} else {
+  console.log(`${FizzBuzzTask} Kritilgan raqam 3 ga ham 5 ga ham bo'linmaydi`);
+}
+
+
+
+// * * * 2-mashq * * * //
+
+// var rundemNumber = Math.round(Math.random() * 5);
+// var userNumber = Number(Math.abs(Math.round(prompt("son kriting"))));
+
+// console.log(`random number: ${rundemNumber}`);
+
+// if(userNumber == ""){
+//     console.log("son kiritishingiz majburiy");
+// }else if(rundemNumber == userNumber){
+//     console.log(`topdingiz siz kiritgan son: ${userNumber} random number bilan bir xil.`);
+// }else{
+//     console.log(`!topolmadingiz siz kiritgan son ${userNumber} random number bilan bir xil emas.`);
+// }
