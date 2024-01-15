166ed326f13839941c930814830a32fdc7c3bd7a
S1ardor <quvondiqovs06@gmail.com>
2024-01-15 21:44:28 +1000

    JS lesson-3

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
 // * * * 1-mashq * * * //
+var elResult = document.querySelector(".js-result");
 
 var FizzBuzzTask = Number(Math.abs(prompt("Son kriting")));
-
 if(FizzBuzzTask == ""){
-    console.log("Son kritishingiz majburiy");
+    elResult.textContent = "Son kritishingiz majburiy";
 }else if (FizzBuzzTask % 3 == 0 && FizzBuzzTask % 5 == 0) {
-  console.log("FizzBUZZ");
+  elResult.textContent = `FizzBuzz: ${FizzBuzzTask}`;
 } else if (FizzBuzzTask % 3 == 0) {
-  console.log("Fizz");
+  elResult.textContent = `Fizz: ${FizzBuzzTask}`;
 } else if (FizzBuzzTask % 5 == 0) {
-  console.log("Buzz");
+  elResult.textContent = `Buzz: ${FizzBuzzTask}`;
 } else {
-  console.log(`${FizzBuzzTask} Kritilgan raqam 3 ga ham 5 ga ham bo'linmaydi`);
+  elResult.textContent = `${FizzBuzzTask} Kritilgan raqam 3 ga ham 5 ga ham bo'linmaydi`;
 }
-
-
-
-// * * * 2-mashq * * * //
-
-// var rundemNumber = Math.round(Math.random() * 5);
-// var userNumber = Number(Math.abs(Math.round(prompt("son kriting"))));
-
-// console.log(`random number: ${rundemNumber}`);
-
-// if(userNumber == ""){
-//     console.log("son kiritishingiz majburiy");
-// }else if(rundemNumber == userNumber){
-//     console.log(`topdingiz siz kiritgan son: ${userNumber} random number bilan bir xil.`);
-// }else{
-//     console.log(`!topolmadingiz siz kiritgan son ${userNumber} random number bilan bir xil emas.`);
-// }
