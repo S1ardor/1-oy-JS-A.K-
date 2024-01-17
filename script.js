891d7c126238fade4b9b467abf385b944068babe
S1ardor <quvondiqovs06@gmail.com>
2024-01-17 12:10:32 +1000

    JS lesson-4

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 // * * * 1-mashq * * * //
-var elResult = document.querySelector(".js-result");
+var elForm = document.querySelector(".js-form");
+var elInput = elForm.querySelector(".js-input");
+var elSelect = document.querySelector(".js-select");
+var elLetter = document.querySelector(".js-letter");
+var button = document.querySelector(".js-button");
 
-var FizzBuzzTask = Number(Math.abs(prompt("Son kriting")));
-if(FizzBuzzTask == ""){
-    elResult.textContent = "Son kritishingiz majburiy";
-}else if (FizzBuzzTask % 3 == 0 && FizzBuzzTask % 5 == 0) {
-  elResult.textContent = `FizzBuzz: ${FizzBuzzTask}`;
-} else if (FizzBuzzTask % 3 == 0) {
-  elResult.textContent = `Fizz: ${FizzBuzzTask}`;
-} else if (FizzBuzzTask % 5 == 0) {
-  elResult.textContent = `Buzz: ${FizzBuzzTask}`;
-} else {
-  elResult.textContent = `${FizzBuzzTask} Kritilgan raqam 3 ga ham 5 ga ham bo'linmaydi`;
-}
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  var inputValue = elInput.value;
+  var selectValue = elSelect.value;
+
+  if (inputValue && selectValue) {
+    var result = inputValue * selectValue;
+    elLetter.textContent = `Sum: ${result}`;
+  } else {
+    elLetter.textContent = "Sum kiritish yoki valyuta tanash majburiy";
+  }
+  //   elForm.reset();
+});
