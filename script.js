61fe2cd78b50780295d87c1436c26cd03337f94e
S1ardor <quvondiqovs06@gmail.com>
2024-01-19 16:08:36 +1000

    JS lesson-8

--- script.js
+++ script.js
@@ -1,78 +1,107 @@
-// 1-Task // birinchi harf kotta yoki kichik ekanligni aniqlash //
-
-var elForm = document.querySelector(".js-form");
-var elInput = elForm.querySelector(".js-input");
+var elInput = document.querySelector(".js-input");
+var elPreButton = document.querySelector(".js-btn");
+var elNextButton = document.querySelector(".js-button");
 var elResult = document.querySelector(".js-result");
+var elPreDelete = document.querySelector(".js-predelete");
+var elNextDelete = document.querySelector(".js-nextdelete");
 
-elForm.addEventListener("submit", function(evt) {
-  evt.preventDefault();
+var cars = ["BMW", "MERS", "AUDI", "BYD"];
+elResult.textContent = cars.join(" ");
+console.log(cars);
 
-  var inputValue = elInput.value.trim();
-if(inputValue == ""){
-  elResult.textContent= "harf kiritishingiz shart";
-}else if(inputValue[0] == inputValue[0].toUpperCase()){
-    elResult.textContent = "birinchi harf kotta!";
-  }else{
-    elResult.textContent = "birinchi harf kichik!";
-  }
+elPreButton.addEventListener("click", function(evt) {
+  var inputValue = elInput.value.trim().toUpperCase();
+  var carsUnshift = cars.unshift(inputValue);
+  elResult.textContent = carsUnshift;
+  elResult.textContent = cars;
 });
 
+elNextButton.addEventListener("click", function (evt) {
+  var inputNextValue = elInput.value.trim().toUpperCase();
+  var carsPush = cars.push(inputNextValue);
+  elResult.textContent = carsPush;
+  elResult.textContent = cars;
+});
 
+// Pre va Next dan delete //
+var inputDeleteValue = elInput.value;
 
-// 2-Task // birinchi va oxirgi harf bir-biriga oxshash yoki oxhsash emasligini topish // 
-
-// var elForm = document.querySelector(".js-form");
-// var elInput = elForm.querySelector(".js-input");
-// var elResult = document.querySelector(".js-result");
-
-// elForm.addEventListener("submit", function(evt) {
-//   evt.preventDefault();
-
-//   var inputValue = elInput.value.trim();
-
-//   if(inputValue[0] == inputValue[inputValue.length -1]){
-//     elResult.textContent = "BINGOO !!!";
-//   }else{
-//     elResult.textContent = "Birinchi va oxirgi harf xar-xil!";
-//   }
-
-// });
-
-
-
-// 3-Task // birinchi son kotta bolsa kichikga kichik bolsa kottaga o'zgartirish ToUpperCase(), ToLowerCase() foydalanib.
-
-// var elForm = document.querySelector(".js-form");
-// var elInput = elForm.querySelector(".js-input");
-// var elResult = document.querySelector(".js-result");
-// var newText = "";
-
-// elForm.addEventListener("submit", function(evt) {
-//   evt.preventDefault();
-//    var inputValue = elInput.value.trim();
-
-//    if(inputValue[0] == inputValue[0].toUpperCase()){
-//     newText = inputValue[0].toLowerCase() + inputValue.slice(1, inputValue.length);
-//    }else if(inputValue[0] == inputValue[0].toLowerCase()){
-//     newText = inputValue[0].toUpperCase() + inputValue.slice(1, inputValue.length);
-//    }
-//    elResult.textContent = newText
-// });
+elPreDelete.addEventListener("click", function(){
+    var preDelete = cars.shift(inputDeleteValue);
+    elResult.textContent = preDelete;
+    elResult.textContent = cars;
+  });
+  
+  elNextDelete.addEventListener("click", function () {
+    var preDelete = cars.pop(inputDeleteValue);
+    elResult.textContent = preDelete;
+    elResult.textContent = cars;
+  });
 
 
 
-// 4-Task // uch marotaba son kritiladi va o'sha sonlardan o'rtanchasini topish.
 
-// var oneNumber = prompt("birinchi son  kriting");
-// var twoNumber = prompt("ikkinchi son kriting");
-// var threeNumber = prompt("uchunchi son kriting");
 
-// if(oneNumber > twoNumber && oneNumber < threeNumber || oneNumber < twoNumber && oneNumber > threeNumber){
-//   console.log(oneNumber);
-// }else if(twoNumber > oneNumber && twoNumber < threeNumber || twoNumber < oneNumber && twoNumber > threeNumber){
-//   console.log(twoNumber);
-// }else if(threeNumber > oneNumber && threeNumber < twoNumber || threeNumber < oneNumber && threeNumber > twoNumber){
-//   console.log(threeNumber)
-// }else{
-//   console.log("xato!");
+
+
+
+
+
+
+// 2-usul css ga etibor berilmagan lekin kod yaxshi chiqgan//
+
+// const elForm = document.querySelector(".js-form");
+// var elInput = document.querySelector(".js-input");
+// var elPreButton = document.querySelector(".js-button");
+// var elNextButton = document.querySelector(".js-btn");
+// var elPreButtonDelete = document.querySelector(".js-delete__pre-button");
+// var elNextButtonDelete = document.querySelector(".js-delete__next-button");
+// var elRender = document.querySelector(".box");
+// const elBtns = document.querySelectorAll(".js-form button");
+
+// var cars = ["BMW", "MERS", "AUDI", "BYD"];
+
+// const elList = document.querySelector(".js-list")
+// let elTemplate = document.querySelector(".js-template").content
+// const handleRenderList = (arr) => {
+//     if(arr.length){
+//         const documentFragment = document.createDocumentFragment()
+//         elList.innerHTML = null
+//         for(const word of arr){
+//             const clone = elTemplate.cloneNode(true)
+//             const elHeading = clone.querySelector("h3")
+//             elHeading.textContent = word
+//             documentFragment.appendChild(clone);
+//         }   
+//         elList.appendChild(documentFragment);
+//     }
 // }
+// const handleClick = (evt) => {
+//     console.log(evt.target.id)
+//     switch(evt?.target?.id){
+//         case "before":{
+//             cars.unshift(elInput.value)
+//         }break;
+//         case "after":{
+//             cars.push(elInput.value)
+//         }break;
+//         case "left":{
+//             cars.shift()
+//         }break;
+//         case "right":{
+//             cars.pop()
+//         }break;
+//         default: return false
+//     }
+//     handleRenderList(cars)
+// }
+// elBtns.forEach((item) => {
+//     item.addEventListener("click", handleClick)
+// })
+// elForm.addEventListener("submit", function(evt){
+   
+//     evt.preventDefault()
+//     var inputValue = elInput.value;
+
+// });
+// handleRenderList(cars);
