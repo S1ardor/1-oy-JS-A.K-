83ce90794258e0b7b2322de594aca2eed69074dd
S1ardor <quvondiqovs06@gmail.com>
2024-01-23 11:54:26 +1000

    JS lesson-10

--- script.js
+++ script.js
@@ -1,38 +1,110 @@
-var elPokeList = document.querySelector(".js-pokelist");
-var elimg = document.querySelector(".js-name");
-var slicePokemons = pokemons.slice(0, 20);
 
-for (var poke of slicePokemons) {
-  var newList = document.createElement("li");
-  var newPokemonNum = document.createElement("span");
-  var newPokeImg = document.createElement("img");
-  var newPokemonName = document.createElement("strong");
-  var newCondyCount = document.createElement("time");
-  var newListType = document.createElement("h6");
-  var newListCandy = document.createElement("p");
+// SINF ISHI //
 
-  newPokemonNum.textContent = poke.num;
-  newPokeImg.src = poke.img;
-  newPokeImg.alt = poke.name + "pokemon qaxramoni";
-  newPokemonName.textContent = poke.name;
-  newCondyCount.textContent = poke.spawn_chance;
-  newListType.textContent = poke.type.join(", ");
-  newListCandy.textContent = poke.candy;
+// 1-Task // arr dagi hamma stringlarni qo'shib qaytarish //
 
-  newList.append(newPokemonNum, newPokeImg, newPokemonName, newCondyCount, newListType, newListCandy);
-  elPokeList.appendChild(newList);
+// const fruits = ["banan", "kivi", "ananas"];
+// let count = 0;
+// for (const fruit of fruits) {
+//   count += fruit.length;
+// }
+// console.log(count);
+
+
+
+// 2-Task // agar prompt dan kiritilgan malumod da "hi", yoki "hello" so'zi ishtirok etgan bo'lsa "assalomu aleykum" so'zi qaytarsin //
+
+// const userLetter = prompt("so'z kriting");
+// if(userLetter.toLowerCase().includes("hi") || userLetter.toLowerCase().includes("hello")){
+//     console.log("asslomu aleykum");
+// }else{
+//     console.log("xato");
+// }
+
+
+
+// 3-task // eng kotta length ni qaytarish //
+
+// const cars = ["BMW", "MERS", "AUDI", "CHEVROLET"];
+// let count = "";
+// for(const car of cars){
+//     if(car.length > count.length){
+//         count = car;
+//     }
+// }
+// console.log(count);
+
+
+
+// 4-Task // birinchi sonni oxiridagi songa qo'shib qaytarish //
+
+// const maxNumber = [1, 34, 553, 12, 52, 776,];
+// const result = maxNumber[0] + maxNumber[maxNumber.length -1];
+// console.log(result);
+
+
+
+// 5-Task // max hariflarni nechtaligini topish toUpperCase() foydalanib // 
+
+// const maxWords = prompt("so'z kriting");
+// let wordsArr = maxWords.split("");
+// let count = 0;
+// for(const Words of wordsArr){
+//     if(Words == Words.toUpperCase()){
+//         count++
+//     }
+// }
+// console.log(count);
+
+
+
+
+// UY ISHI //
+    
+// 1-Task //
+
+const elForm = document.querySelector(".js-form");
+const elInput = elForm.querySelector(".js-input");
+const elResult = document.querySelector(".js-result");
+
+function wordFind() {  
+    elForm.addEventListener("submit", function(evt) {
+        evt.preventDefault();
+        const inputValue = elInput.value.toUpperCase().trim().split("");
+        let count = 0;
+        const resInpValue = inputValue[inputValue.length -1];
+        for(const iterator of inputValue){
+            if(iterator === resInpValue){
+                count++
+            }
+            elResult.textContent = `o'shash harf: ${count}`;
+        }
+    }); 
+
 }
+wordFind();
 
 
 
+// 2-Task //
 
-// maximum sonni topish formulasi //
+// undefined ??? //
 
-var numbers = [1, 4, 6, 10, 3, 8, 9];
-var result = 0;
-for(var num of numbers){
-  if(num > result){
-    result = num;
-  }
-}
-console.log(result);
+
+// 3-task //
+
+// const handlWord = (arr) => {
+//     let result = "";
+//     for(let  i = 0; i<arr.length; i++){
+//         for(let si = 0; si <arr[i].length; si++){
+//             if(si === 0 || si === arr[i].length-1){
+//                 result += arr[i][si].toUpperCase()
+//             }else{
+//                 result += arr[i][si]
+//             }
+//         }
+//     }
+//     return result
+// }
+// let result = handlWord(["banan"]);
+// console.log(result);
